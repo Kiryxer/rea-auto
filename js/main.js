fa910ef793c9c5cd48a0386727e046237a17a9aa
Kiryxer <55143725+Kiryxer@users.noreply.github.com>
2019-11-11 21:11:53 +0300

    Были сделаны формы заказа, правка страницы контактов
    Были сделаны модальные окна для заказа пропусков, подправлена страница контактов

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 $(function(){
 
-$('.slider__inner, .news__slider-inner').slick({
-  nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-  prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-  infinite: false
-});
-
-$('select').styler();
-
-
 $('.header__btn-menu').on('click', function(){
   $('.menu ul').slideToggle();
 })
