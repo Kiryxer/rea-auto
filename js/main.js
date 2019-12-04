d32d3cf1b3831d140067d99ee7e71f36abd250e4
Kiryxer <55143725+Kiryxer@users.noreply.github.com>
2019-12-04 16:19:53 +0300

    все

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-$(function(){
+$(function () {
 
-$('.header__btn-menu').on('click', function(){
-  $('.menu ul').slideToggle();
-})
+  $('.main-menu__btn').swipe({
+    swipeStatus: function (event, phase, direction, distance, duration, fingers) {
+      if (phase == "move" && direction == "right") {
+        $('.menu').animate({
+          left: '0px'
+        }, 200);
+      }
+      if (phase == "move" && direction == "left") {
+        $('.menu').animate({
+          left: '-310px'
+        }, 200);
+      }
+    }
+  });
 
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  })
 });
